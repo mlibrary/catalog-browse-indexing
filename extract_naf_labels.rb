cabe602f6ee2b40516a449b4fb55dc81a8ae1cf2
Bill Dueber <bill@dueber.com>
2022-05-31 15:22:59 -0400

    Add rudimentary solr config

--- extract_naf_labels.rb
+++ extract_naf_labels.rb
@@ -5,77 +5,178 @@ require 'json'
 filename = ARGV.shift
 filename ||= "../lcnaf.skos.ndjson"
 
-class NAFEntry
-  attr_accessor :graph
-  attr_reader :orig
-  def initialize(str)
-    @orig = str
-    @graph = JSON.parse(str)['@graph']
-  end
+module NAF
 
-  def deprecated?
-    @deprecated ||= main.nil? and @graph.find{|x| x["cs:changeReason"] == "deprecated"}
-  end
+  FS = "\u001c" # Field separator
 
-  # Remove punctuation that might interfere with matches against MARC data
-  def clean_up(str)
-    str.gsub(/[,.;]/, '').downcase
-  end
+  class Entry
+    def self.new(str)
+      orig = str
+      graph = JSON.parse(str)['@graph']
+      main = graph.find { |x| x.dig('skos:inScheme', '@id') == "http://id.loc.gov/authorities/names" }
 
-
-  def id
-    if deprecated?
-    else
-      main['@id'].split('/').last
+      if main.nil?
+        RedirctEntry.new(orig, graph)
+      else NormalEntry.new(orig, graph)
+      end
     end
-  rescue => e
-    require 'pry'; binding.pry
+
   end
 
-  # TODO
-  def main
-    return @main if @main
-    normal = @graph.find { |x| x.dig('skos:inScheme', '@id') == "http://id.loc.gov/authorities/names" }
-    @main = normal
-  end
+  class NormalEntry
+    attr_accessor :graph, :orig, :main
 
-  def prefLabel
-    main["skos:prefLabel"]
-  end
-
-  def other_labels
-    [altLabels, skosx_labels].flatten.uniq.compact
-  end
-
-  def skosx_labels
-    items = main["skosxl:altLabel"]
-    items = [items] if items.kind_of? Hash
-    if items
-      ids = Array(items).map{|x| x["@id"]}
-      ids.map{|id| skosx_label_by_id(id)}
-    else
-      []
+    def type
+      :entry
     end
-  rescue => e
-    require 'pry'; binding.pry
+
+    def initialize(orig, graph)
+      @orig = orig
+      @graph = graph
+      @main = @graph.find { |x| x.dig('skos:inScheme', '@id') == "http://id.loc.gov/authorities/names" }
+    end
+
+    def deprecated?
+      @deprecated ||= main.nil? and @graph.find { |x| x["cs:changeReason"] == "deprecated" }
+    end
+
+    # Remove punctuation that might interfere with matches against MARC data
+    def clean_up(str)
+      str.gsub(/[,.;]\Z/, '')
+    end
+
+    def id
+      id_from_uri(main['@id'])
+    rescue => e
+      require 'pry'; binding.pry
+    end
+
+    def prefLabel
+      main["skos:prefLabel"]
+    end
+
+    def other_labels
+      [altLabels, skosx_labels].flatten.uniq.compact
+    end
+
+    def skosx_labels
+      items = main["skosxl:altLabel"]
+      items = [items] if items.kind_of? Hash
+      if items
+        ids = Array(items).map { |x| x["@id"] }
+        ids.map { |id| skosx_label_by_id(id) }
+      else []
+      end
+    rescue => e
+      require 'pry'; binding.pry
+    end
+
+    def skosx_label_by_id(id)
+      @graph.find { |x| x['@id'] == id }["skosxl:literalForm"]
+    end
+
+    def altLabels
+      Array(main["skos:altLabel"])
+    end
+
+    def id_from_uri(str)
+      str.split("/").last
+    end
+
+    # id = naf.id
+    # pclean = naf.clean_up(naf.prefLabel)
+    # puts [id, naf.prefLabel, pclean, 'p'].join("\t")
+    # naf.other_labels.each do |l|
+    #   lclean = naf.clean_up(l)
+    #   puts [id, l, lclean, 'a'].join("\t")
+
+    def to_json
+      {type: type, id: id, label: prefLabel, alternate_labels: other_labels}.to_json
+    end
+
+    def to_tsv
+      [prefLabel, id, type, other_labels.join(FS)].join("\t")
+    end
+
   end
 
-  def skosx_label_by_id(id)
-    @graph.find { |x| x['@id'] == id }["skosxl:literalForm"]
-  end
+  class RedirctEntry < NormalEntry
 
-  def altLabels
-    Array(main["skos:altLabel"])
+    def type
+      if changeStructure
+        :see_instead
+      else
+        :deprecation
+      end
+    end
+
+    def initialize(orig, graph)
+      @orig = orig
+      @graph = graph
+    end
+
+    def changeStructure
+      @cs ||= @graph.find { |x| x["rdfs:seeAlso"] }
+    end
+
+    def see_also_uris
+      rdfssa = changeStructure["rdfs:seeAlso"]
+      sa = case rdfssa
+           when Hash
+             [rdfssa]
+           when Array
+             rdfssa
+           else raise "rdfssa is a #{rdfssa.class}, whcih it shouldn't be."
+           end
+      sa.map { |x| x["@id"] }.compact
+    rescue
+      require 'pry'; binding.pry
+    end
+
+    def prefLabel
+      changeStructure["skosxl:literalForm"]
+    end
+
+    def uri
+      changeStructure["@id"]
+    end
+
+    def id
+      id_from_uri(uri)
+    end
+
+    def targets
+      see_also_uris.map do |uri|
+        label = @graph.find { |x| x["@id"] == uri and x["@type"] == "skos:Concept" and x["skos:prefLabel"] }["skos:prefLabel"]
+        next unless label
+        [id_from_uri(uri), uri, label]
+      end
+    end
+
+    def to_json
+      {type: type, id: id, label: prefLabel, targets: targets}.to_json
+    end
+
+    NO_OTHER_LABELS = ""
+    def to_tsv
+      [prefLabel, id, type, NO_OTHER_LABELS, targets.map{|x| x.join("|")}.join(FS)].join("\t")
+    end
   end
 end
 
+puts %w[label id type alternate_labels targets].join("\t")
+File.open(filename, 'r:utf-8').each_with_index do |line, i|
+  naf = NAF::Entry.new(line)
+  next if naf.type == :deprecation
+  puts naf.to_tsv
+end
 
-File.open(filename, 'r:utf-8').each do |line|
-  naf = NAFEntry.new(line)
-  if naf.main.nil?
-    STDERR.puts naf.orig
-    next
-  end
+__END__
+
+
+
+
+
   id = naf.id
   pclean = naf.clean_up(naf.prefLabel)
   puts [id, naf.prefLabel, pclean, 'p'].join("\t")
@@ -83,6 +184,4 @@ File.open(filename, 'r:utf-8').each do |line|
     lclean = naf.clean_up(l)
     puts [id, l, lclean, 'a'].join("\t")
   end
-end
-
 
