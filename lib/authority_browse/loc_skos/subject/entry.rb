86f63280cd5a1135d02ceb9655b533b601d16d06
Bill Dueber <bill@dueber.com>
2022-08-18 13:55:31 -0400

    Reduce to one entry type; add in reverse-lookups for seeAlso entries

--- lib/authority_browse/loc_skos/subject/entry.rb
+++ lib/authority_browse/loc_skos/subject/entry.rb
@@ -6,26 +6,14 @@ require "json"
 
 module AuthorityBrowse::LocSKOSRDF
   module Subject
-    class Entry
+    class Entry < GenericEntry
 
       attr_accessor :components
 
-      def self.new(skos_hash)
-        if GenericEntry.has_see_also?(skos_hash)
-          SeeAlsoEntry.new(skos_hash)
-        else
-          ConceptEntry.new(skos_hash)
-        end
-      end
-    end
-
-    class ConceptEntry < GenericEntry
-
       # Freeze these 'cause they'll be used over and over again
-      JSON_CREATE_ID = JSON.create_id.freeze
       ConceptEntryName = self.name.freeze
 
-      attr_accessor :category, :narrower, :broader, :components
+      attr_accessor :category, :narrower, :broader, :components, :see_also, :incoming_see_also
 
       def initialize(skos_hash)
         @namespace = "http://id.loc.gov/authorities/subjects"
@@ -35,6 +23,8 @@ module AuthorityBrowse::LocSKOSRDF
         @category = "subject"
         @narrower = build_references(narrower_ids)
         @broader = build_references(broader_ids)
+        @see_also = build_references(see_also_ids)
+        @incoming_see_also = {}
       rescue NoMethodError => e
         require 'pry'; binding.pry
       end
@@ -46,7 +36,7 @@ module AuthorityBrowse::LocSKOSRDF
       end
 
       def xref_ids
-        broader_ids.union(narrower_ids)
+        broader_ids.union(narrower_ids).union(see_also_ids)
       end
 
       def pare_down_components!
@@ -54,9 +44,7 @@ module AuthorityBrowse::LocSKOSRDF
         @components.select! { |id, c| in_namespace?(id) or xref_ids.include?(id) }
       end
 
-      # Try to build id/label pairs for a set of ids (for narrower/broader). Lucky for us, the LoC only
-      # _sometiems_ includes the necessary information in another component, so we'll side-effect setting
-      # a flag that says we'll need to fill things in during another pass.
+      # Try to build id/label pairs for a set of ids (for narrower/broader).
       def build_references(ids)
         rv = {}
         ids.each do |id|
@@ -67,14 +55,14 @@ module AuthorityBrowse::LocSKOSRDF
         rv
       end
 
-      def see_also?
-        false
+      def label
+        main.label
       end
 
-      alias_method :label, :pref_label
-
       def needs_xref_lookups?
-        broader.keys.size != broader_ids.size or narrower.keys.size != narrower_ids.size
+        broader.keys.size != broader_ids.size or
+          narrower.keys.size != narrower_ids.size or
+          see_also.keys.size != see_also_ids.size
       end
 
       def broader_ids
@@ -83,6 +71,10 @@ module AuthorityBrowse::LocSKOSRDF
 
       def narrower_ids
         collect_relevant_ids("skos:narrower")
+      end
+
+      def see_also_ids
+        collect_relevant_ids("rdfs:seeAlso")
       end
 
       # A "relevant" id is one in our @namespace or one that we already have a component for
@@ -98,12 +90,17 @@ module AuthorityBrowse::LocSKOSRDF
         @broader[id] = label
       end
 
+      # Only add in a redirect if the text is different than the deleted record's label. If
+      # it is, the new record will have been found anyway.
+      def add_see_also(id, text)
+        @see_also[id] = text unless text == label
+      end
+
       # We need to resolve any missing xrefs through the use of an object (like Subjects)
       # that responds to o[id] with an entry
-
       def resolve_xrefs!(lookup_table)
         return unless needs_xref_lookups?
-        (broader_ids - components.keys).each do |xid|
+        (broader_ids - broader.keys).each do |xid|
           e = lookup_table[xid]
           if e
             add_broader(xid, e.label)
@@ -111,7 +108,7 @@ module AuthorityBrowse::LocSKOSRDF
             warn "Entry #{id} can't find broader xref #{xid}"
           end
         end
-        (narrower_ids - components.keys).each do |xid|
+        (narrower_ids - narrower.keys).each do |xid|
           e = lookup_table[xid]
           if e
             add_narrower(xid, e.label)
@@ -119,7 +116,30 @@ module AuthorityBrowse::LocSKOSRDF
             warn "Entry #{id} can't find narrower xref #{xid}"
           end
         end
+        (see_also_ids - see_also.keys).each do |xid|
+          e = lookup_table[xid]
+          if e
+            add_see_also(xid, e.label) unless see_also.has_key?(xid)
+            e.incoming_see_also[id] = label
+          else
+            warn "Entry #{id} can't find seeAlso xref #{xid}"
+          end
+        end
       end
+
+      def to_solr_doc
+        {
+          id: AuthorityBrowse.alphajoin(label, id),
+          subject: label,
+          narrower: narrower.values,
+          broader: broader.values,
+          see_also: see_also.values,
+          incoming_see_also: incoming_see_also.values,
+          browse_field: "subject",
+          json: {id: id, subject: label, narrower: narrower, broader: broader, see_also: see_also, incoming_see_also: incoming_see_also}.to_json
+        }
+      end
+
 
       def to_json(*args)
         {
@@ -129,6 +149,8 @@ module AuthorityBrowse::LocSKOSRDF
           narrower: @narrower,
           broader: @broader,
           components: @components,
+          see_also: @see_also,
+          incoming_see_also: @incoming_see_also,
           need_xref: needs_xref_lookups?,
           JSON_CREATE_ID => ConceptEntryName
         }.to_json(*args)
@@ -140,76 +162,12 @@ module AuthorityBrowse::LocSKOSRDF
         e.category = rec["category"]
         e.narrower = rec["narrower"]
         e.broader = rec["broader"]
-        e.components = rec["components"]
-        e.set_main!
-        e
-      end
-
-    end
-
-    class SeeAlsoEntry < ConceptEntry
-      SEEALSOENTRYNAME = self.name
-
-      attr_accessor :see_also
-
-      def initialize(*args, **kwargs)
-        super
-        @see_also = build_references(see_also_ids)
-        @category = "see_also"
-      end
-
-      def see_also?
-        true
-      end
-
-      def needs_xref_lookups?
-        see_also_ids.size != see_also.size
-      end
-
-      def label
-        main.label
-      end
-
-      def see_also_ids
-        collect_relevant_ids("rdfs:seeAlso")
-      end
-
-      # Only add in a redirect if the text is different than the deleted record's label. If
-      # it is, the new record will have been found anyway.
-      def add_see_also(id, text)
-        @see_also[id] = text unless text == label
-      end
-
-      def resolve_xrefs!(lookup_table)
-        (see_also_ids - components.keys).each do |xid|
-          e = lookup_table[xid]
-          if e
-            add_see_also(xid, e.label)
-          else
-            warn "Entry #{id} can't find seeAlso xref #{xid}"
-          end
-        end
-      end
-
-      def to_json(*args)
-        {
-          id: id,
-          category: category,
-          label: label,
-          see_also: see_also,
-          components: components,
-          JSON_CREATE_ID => SEEALSOENTRYNAME
-        }.to_json(*args)
-      end
-
-      def self.json_create(rec)
-        e = allocate
-        e.components = rec["components"]
         e.see_also = rec["see_also"]
+        e.incoming_see_also = rec["incoming_see_also"]
+        e.components = rec["components"]
         e.set_main!
         e
       end
-
     end
   end
 end
--- lib/authority_browse/subject.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "loc_skos/subject/subjects"
-
