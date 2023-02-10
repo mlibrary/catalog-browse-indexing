9272c672a94c1dbb5cb15fdf8975cbc7a82fbacc
Bill Dueber <bill@dueber.com>
2023-02-10 13:41:33 -0500

    Ditch some dead stuff

--- traject/writers/json_writer.rb
+++
@@ -1,8 +0,0 @@
-require "traject"
-require "traject/json_writer"
-
-settings do
-  store "writer_class_name", "Traject::JsonWriter"
-  store "output_file", "debug.json"
-  provide "processing_thread_pool", 5
-end
