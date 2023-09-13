e56414c75292a43d7b704a8e0ea3e2dffff3cb12
Monique Rio <mrio@umich.edu>
2023-09-13 09:46:54 -0400

    WIP integration specs

--- bin/names/generate_solr_docs.sh
+++ bin/names/generate_solr_docs.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 local_dir=`dirname "${0}"`
+
+# the directory with scripts
 bindir=`realpath ${local_dir}/..`
+
+# the root directory
 rootdir=`realpath "${bindir}/.."`
+
+# the library of congress info directory
 locdir="${rootdir}/data/LoC"
 
 today=`date +"%Y%m%d"`
 namesdir="${rootdir}/data/names/${today}"
 
 mkdir -p $namesdir
+
+#aab = author authoritative browse" 
 aab="${namesdir}/aab.tsv.gz"
+
+# this is a seqel db 
 db="${rootdir}/data/authorities.db"
+
+# this is matched something or other
 matched="${namesdir}/matched.jsonl.gz"
+
+# this is unmatched something or other
 unmatched="${namesdir}/unmatched.jsonl.gz"
 
+# this is a field in biblio
 name_field="author_authoritative_browse"
 
 echo "Exporting ${name_field} stats from biblio into ${aab}"
