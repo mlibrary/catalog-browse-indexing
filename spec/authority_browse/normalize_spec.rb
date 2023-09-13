f20f8a449a8306bb35f938707dc38e2f8bcb2d8e
Monique Rio <mrio@umich.edu>
2023-09-13 17:19:04 -0400

    added specs to verify icu is getting used

--- spec/authority_browse/normalize_spec.rb
+++ spec/authority_browse/normalize_spec.rb
@@ -3,7 +3,16 @@ RSpec.describe AuthorityBrowse::Normalize do
     it "will remove the leading 'the'" do
       expect(described_class.match_text("the cat in the hat")).to eq("cat in the hat")
     end
-    xit "exercises the jruby unicode normalization" do
+    context "unicode normalization with icu" do
+      it "handles case folding" do
+        # \u0393 = Γ
+        # \u03b3 = γ
+        expect(described_class.match_text("CAT \u0393")).to eq("cat \u03b3")
+      end
+      # unicode makes: ḱṷṓn
+      it "collapses compound characters and changes to ascii to one character" do
+        expect(described_class.match_text("\u006B\u0301\u0075\u032D\u006F\u0304\u0301\u006E")).to eq("kuon")
+      end
     end
     it "removes spaces around a double dash" do
       expect(described_class.match_text("cat -- in")).to eq("cat--in")
@@ -11,8 +20,8 @@ RSpec.describe AuthorityBrowse::Normalize do
     it "changes : and - to spaces" do
       expect(described_class.match_text("cat:in-:-the -hat:")).to eq("cat in the hat")
     end
+    
     # https://ruby-doc.org/core-3.0.0/Regexp.html#class-Regexp-label-Character+Properties
-    #
     it "changes Unicode Punctuation Character set to empty string" do
       expect(described_class.match_text("cat¶§;in")).to eq("catin")
     end
