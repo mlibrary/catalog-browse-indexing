0e92c92ab555ae299eb54610f8242b023e01a146
Monique Rio <mrio@umich.edu>
2023-09-18 16:19:49 -0400

    adds specs for skos_to_dumpfile.rb

---
+++ spec/integrations/skos_to_dumpfile_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../../bin/subjects/skos_to_dumpfile.rb"
+RSpec.describe SkosToDumpWrapper do
+  before(:each) do
+    @skos_file = "spec/fixtures/civil_war.json.gz"
+    @dumpfile = "tmp/dumpfile.jsonl.gz"
+  end
+  it "runs something" do
+    expect(File.exist?(@dumpfile)).to eq(false)
+    described_class.run(@skos_file, @dumpfile)
+    expect(File.exist?(@dumpfile)).to eq(true)
+  end
+  after(:each) do
+    `rm tmp/*`
+  end
+end
