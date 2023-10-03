c418d3bab2434131f61ad65e6b5254c5cb87395c
Monique Rio <mrio@umich.edu>
2023-10-03 09:22:14 -0400

    add some db seed

--- bin/seed.rb
+++ bin/seed.rb
@@ -8,24 +8,29 @@ require "authority_browse"
 require "json"
 
 mark_twain = JSON.parse(File.read("spec/fixtures/loc_authorities/mark_twain_skos.json"))
-louis_de_conte_skos = JSON.parse(File.read("spec/fixtures/loc_authorities/louis_de_conte_skos.json"))
-AuthorityBrowse::LocAuthorities::Entry.new(mark_twain).save_to_db
-AuthorityBrowse::LocAuthorities::Entry.new(louis_de_conte_skos).save_to_db
+louis = JSON.parse(File.read("spec/fixtures/loc_authorities/louis_de_conte_skos.json"))
+mark_twain_entry = AuthorityBrowse::LocAuthorities::Entry.new(mark_twain)
+louis_entry = AuthorityBrowse::LocAuthorities::Entry.new(louis)
+
+db = AuthorityBrowse.authorities_graph_db
+db[:names].insert(id: mark_twain_entry.id, label: mark_twain_entry.label)
+db[:names].insert(id: louis_entry.id, label: louis_entry.label)
+db[:names_see_also].insert(name_id: mark_twain_entry.id, see_also_id: louis_entry.id)
 
 # some stuff for testing
-AuthorityBrowse.setup_terms_db
+# AuthorityBrowse.setup_terms_db
 
-terms_db = AuthorityBrowse.terms_db[:names]
-terms_db.insert(term: "Twain, Mark, 1835-1910", count: 7)
-terms_db.insert(term: "Conte, Louis de, 1835-1910", count: 2)
+# terms_db = AuthorityBrowse.terms_db[:names]
+# terms_db.insert(term: "Twain, Mark, 1835-1910", count: 7)
+# terms_db.insert(term: "Conte, Louis de, 1835-1910", count: 2)
 
-docs = Name.all.map do |name|
-  AuthorityBrowse::AuthorityGraphSolrDocument.new(name).to_solr_doc
-end
+# docs = Name.all.map do |name|
+# AuthorityBrowse::AuthorityGraphSolrDocument.new(name).to_solr_doc
+# end
 
-# solr_uploader = AuthorityBrowse::SolrUploader.new(url: "http://solr:SolrRocks@solr:8983/solr/authority_browse/update")
-# docs = File.readlines("/app/bin/docs.json")
+## solr_uploader = AuthorityBrowse::SolrUploader.new(url: "http://solr:SolrRocks@solr:8983/solr/authority_browse/update")
+## docs = File.readlines("/app/bin/docs.json")
 
-# puts docs
-solr_uploader = AuthorityBrowse::SolrUploader.new(collection: "authority_browse")
-solr_uploader.upload docs
+## puts docs
+# solr_uploader = AuthorityBrowse::SolrUploader.new(collection: "authority_browse")
+# solr_uploader.upload docs
