bd7fd28f23b3a2040ff2c5bd027920fa6aaa6509
Monique Rio <mrio@umich.edu>
2023-10-06 16:25:10 -0400

    WIP load counts uses sql statments

--- bin/load_counts.rb
+++ bin/load_counts.rb
@@ -2,59 +2,54 @@ require "pathname"
 $LOAD_PATH.unshift (Pathname.new(__dir__).parent + "lib").to_s
 
 require "authority_browse"
-require "milemarker"
 require "logger"
 require "byebug"
 
-@names = AuthorityBrowse.db[:names]
-@from_biblio = AuthorityBrowse.db[:names_from_biblio]
-
-# @match_text_match = @names.select(:id).where(match_text: :$match_text, deprecated: false).prepare(:select, :match_text_match)
-
-# @deprecated_match = @names.select(:id).where(match_text: :$match_text, deprecated: true).prepare(:select, :match_text_dep_match)
-
-@increase_count = @names.where(id: :$id).prepare(:update, :increase_count, count: (Sequel[:count] + :$count))
-
-def best_match(unmatched)
-  resp = @names.select(:id).where(match_text: unmatched, deprecated: 0)
-  if resp.count > 0
-    resp
-  else
-    @names.select(:id).where(match_text: unmatched, deprecated: 1)
-  end
+def duration(seconds)
+  t = seconds
+  "%02d:%02d:%02d:%02d" % [t / 86400, t / 3600 % 24, t / 60 % 60, t % 60]
 end
+logger = Logger.new($stdout)
 
-# query = "select match_text, sum(count) as total_count from names_from_biblio group by match_text;"
+logger.info "Zeroing out all the counts from the last run. Can take 5min."
+start = Time.now
+AuthorityBrowse.db.transaction { AuthorityBrowse.db[:names].update(count: 0) }
+finish = Time.now
+logger.info "finished zeroing out counts"
+logger.info "duration: #{duration(finish - start)}"
 
-require "concurrent"
+# update names with counts from names_from_biblio
+update_names_counts_statement = <<~SQL
+  UPDATE names AS n
+  SET count = (
+    SELECT sum(count) 
+    FROM names_from_biblio AS nfb
+    WHERE n.match_text = nfb.match_text);
+SQL
 
-threads = 4
-pool = Concurrent::ThreadPoolExecutor.new(
-  min_threads: threads,
-  max_threads: threads,
-  max_queue: 200,
-  fallback_policy: :caller_runs
-)
+logger.info "updating names with counts"
+start = Time.now
+AuthorityBrowse.db.run(update_names_counts_statement)
+finish = Time.now
+logger.info "finished updating names with counts"
+logger.info "duration: #{duration(finish - start)}"
 
-milemarker = Milemarker.new(name: "Match and add counts to db", logger: Logger.new($stdout), batch_size: 1_000)
-milemarker.log "Zeroing out all the counts from the last run. Can take 5mn."
-@names.db.transaction { @names.update(count: 0) }
+## update names_from_biblio with corresponding ids from names
+# statement = <<-SQL
+# UPDATE names_from_biblio as nfb
+# SET name_id = (
+# SELECT n.id
+# FROM names AS n
+# INNER JOIN names_from_biblio AS nfb_inner
+# WHERE n.match_text = nfb_inner.match_text
+# AND n.match_text = nfb.match_text
+# LIMIT 1
+# );
+# SQL
 
-milemarker.threadsafify!
-
-milemarker.log "start loading counts"
-@from_biblio.each do |row|
-  pool.post(row) do |value|
-    resp = best_match(value[:match_text])
-    if resp.count > 0
-      id = resp.first[:id]
-      @increase_count.call(id: id, count: value[:count])
-      @from_biblio.where(id: value[:id]).update(name_id: id)
-    end
-    milemarker.increment_and_log_batch_line
-  end
-end
-
-pool.shutdown
-pool.wait_for_termination
-milemarker.log_final_line
+# UPDATE names_from_biblio AS nfb
+# SET name_id = (
+# SELECT id
+# FROM names AS n
+# WHERE n.match_text = nfb.match_text
+# LIMIT 1);"
