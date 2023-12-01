89c457b4d2a1e03cffaf8f35f4dfc72490a58326
Monique Rio <mrio@umich.edu>
2023-12-01 08:57:16 -0500

    fixes for using SolrCloud gem

--- lib/authority_browse/solr.rb
+++ lib/authority_browse/solr.rb
@@ -75,14 +75,14 @@ module AuthorityBrowse
     #
     # @return[Nil]
     def self.set_daily_reindex_alias
-      S.solrcloud.create_alias(name: reindex_alias, collection_name: collection_name)
+      S.solrcloud.create_alias(name: reindex_alias, collection_name: collection_name, force: true)
     end
 
     # This sets the production alias to today's collection.
     #
     # @return[Nil]
     def self.set_production_alias
-      S.solrcloud.create_alias(name: production_alias, collection_name: collection_name)
+      S.solrcloud.create_alias(name: production_alias, collection_name: collection_name, force: true)
     end
 
     # This verifies that today's collection has enough documents in it. For now
@@ -98,9 +98,9 @@ module AuthorityBrowse
     # than the newest three authority_browse collections.
     #
     # @return[Nil]
-    def self.prune_old_collections
+    def self.prune_old_collections(keep: 3)
       S.logger.info "Pruning the following collections: #{list_old_collections}"
-      list_old_collections.map { |c| S.solrcloud.connection(c) }.each do |coll|
+      list_old_collections(keep: keep).each do |coll|
         coll.delete!
       end
     end
@@ -108,15 +108,14 @@ module AuthorityBrowse
     # Lists the authority_browse collections that are older than the newest
     # three authority_browse collections
     #
-    # @param list [Array] Array of all SolrCloud collections
+    # @param list [Array]<SolrCloud::Collection> Array of all SolrCloud collections
     # @param keep [Integer] how many versions to keep, even if they're old
-    # @return [Array] Array of old authority browse Solrcloud collection
-    # strings
-    def self.list_old_collections(list = S.solrcloud.collections, keep: 3)
+    # @return [Array]<SolrCloud::Collection> Array of old authority browse Solrcloud collections
+    def self.list_old_collections(list: S.solrcloud.collections, keep: 3)
       list.select do |item|
-        item.match?("authority_browse")
+        item.name.match?("authority_browse")
       end.sort do |a, b|
-        Date.parse(a.split("_").last) <=> Date.parse(b.split("_").last)
+        Date.parse(a.name.split("_").last) <=> Date.parse(b.name.split("_").last)
       end[0..(0 - keep - 1)]
     end
   end
--- spec/authority_browse/solr_spec.rb
+++ spec/authority_browse/solr_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe AuthorityBrowse::Solr do
   context "#list_old_collections" do
-    before(:all) do
+    before(:each) do
       @list = [
         "something_11d2069_2023-11-16",
         "something_11d2069_2023-11-15",
@@ -11,17 +11,17 @@ RSpec.describe AuthorityBrowse::Solr do
         "authority_browse_11d2069_2023-11-13",
         "authority_browse_11d2069_2023-11-15",
         "authority_browse_11d2069_2023-11-14"
-      ]
+      ].map { |x| instance_double(SolrCloud::Collection, name: x) }
     end
     it "returns authority_browse collections older than the newest three" do
-      expect(described_class.list_old_collections(@list)).to eq([
+      expect(described_class.list_old_collections(list: @list).map { |x| x.name }).to eq([
         "authority_browse_1.0.1_2023-11-13",
         "authority_browse_11d2069_2023-11-13"
       ])
     end
 
     it "returns old collections with a custom keep_at_least" do
-      expect(described_class.list_old_collections(@list, keep: 4)).to eq(["authority_browse_1.0.1_2023-11-13"])
+      expect(described_class.list_old_collections(list: @list, keep: 4).map { |x| x.name }).to eq(["authority_browse_1.0.1_2023-11-13"])
     end
   end
 end
